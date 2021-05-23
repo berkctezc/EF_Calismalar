32a7a2c9ecbc142c412e37a29d0225c0e7c29110
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 18:15:57 +0300

    updated models

--- EfCodeFirst/Entity/Category.cs
+++ EfCodeFirst/Entity/Category.cs
@@ -5,7 +5,8 @@ namespace EfCodeFirst.Entity
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Tag { get; set; }
+        public string Detail { get; set; }
         //bir kategoride çok ürün olacaktır o yüzden collection olarak tuttuk
         public ICollection<Product> Products { get; set; }
     }
--- EfCodeFirst/Entity/Customer.cs
+++ EfCodeFirst/Entity/Customer.cs
@@ -6,6 +6,6 @@ namespace EfCodeFirst.Entity
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string MusteriSehir { get; set; }
+        public string City { get; set; }
     }
 }
