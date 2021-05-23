a35851b9ff1a308989b0cb3033a5a0dd2bde5e09
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 18:16:10 +0300

    added migrations

---
+++ EfCodeFirst/Migrations/202105231509172_mig14.cs
@@ -0,0 +1,20 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig14 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Title", c => c.String());
+            DropColumn("dbo.Customers", "Surname");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Customers", "Surname", c => c.String());
+            DropColumn("dbo.Customers", "Title");
+        }
+    }
+}
---
+++ EfCodeFirst/Migrations/202105231512104_updated_state.cs
@@ -0,0 +1,26 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updated_state : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "Detail", c => c.String());
+            AddColumn("dbo.Customers", "Surname", c => c.String());
+            AddColumn("dbo.Customers", "City", c => c.String());
+            DropColumn("dbo.Customers", "Title");
+            DropColumn("dbo.Customers", "MusteriSehir");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Customers", "MusteriSehir", c => c.String());
+            AddColumn("dbo.Customers", "Title", c => c.String());
+            DropColumn("dbo.Customers", "City");
+            DropColumn("dbo.Customers", "Surname");
+            DropColumn("dbo.Categories", "Detail");
+        }
+    }
+}
