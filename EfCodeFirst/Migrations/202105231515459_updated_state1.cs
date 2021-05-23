37ccad8e0f7bf9073874bd683f27cb4833df0fad
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 18:17:29 +0300

    changing column name without data loss

---
+++ EfCodeFirst/Migrations/202105231515459_updated_state1.cs
@@ -0,0 +1,22 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updated_state1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "Tag", c => c.String());
+            Sql("Update Categories Set Name=Tag");
+            DropColumn("dbo.Categories", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Categories", "Name", c => c.String());
+            Sql("Update Categories Set Tag=Name");
+            DropColumn("dbo.Categories", "Tag");
+        }
+    }
+}
