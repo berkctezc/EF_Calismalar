535ad389b8082ad2687911a8245f8a98f4585b60
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 18:09:03 +0300

    updated model

---
+++ EfCodeFirst/Migrations/202105231508095_mig13.cs
@@ -0,0 +1,18 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig13 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "MusteriSehir", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "MusteriSehir");
+        }
+    }
+}
