2829fc42a341a6f2ef66e1154c60c95d13a53389
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 17:38:32 +0300

    migrations

--- EfCodeFirst/Migrations/Configuration.cs
+++ EfCodeFirst/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 ﻿namespace EfCodeFirst.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EfCodeFirst.Entity.Context>
     {
