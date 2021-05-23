561a83e1b46d382ae80d0776be33c56aa25c2c32
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 17:44:33 +0300

    migration of customer added

--- EfCodeFirst/Entity/Context.cs
+++ EfCodeFirst/Entity/Context.cs
@@ -4,7 +4,7 @@ namespace EfCodeFirst.Entity
 {
     /* CodeFirst Aşamaları
      * EF yüklenir, Entityler oluşturulur
-     * Context oluşturulur ve bağlamalar yapılır
+     * Context oluşturulur ve entityler ile bağlamalar yapılır
      * appconfig/ webconfigde connectrionstrings>add parametreleri eklenir
      * enable-migrations , add-migration , update-database
      */
@@ -12,6 +12,7 @@ namespace EfCodeFirst.Entity
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
     }
 }
---
+++ EfCodeFirst/Migrations/202105231444017_mig12.cs
@@ -0,0 +1,27 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig12 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Surname = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
