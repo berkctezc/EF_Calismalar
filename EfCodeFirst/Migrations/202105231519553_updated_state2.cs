db530d5eba7a452829efab4b3a321006de80e891
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 18:22:44 +0300

    migrations

---
+++ EfCodeFirst/Migrations/202105231519553_updated_state2.cs
@@ -0,0 +1,18 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updated_state2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Categories", "Detail");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Categories", "Detail", c => c.String());
+        }
+    }
+}
