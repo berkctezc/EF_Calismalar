66f5647d6794ecf01d461b25d63b3dc361c91038
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-23 17:29:02 +0300

    migrations codefirst

---
+++ EfCodeFirst/Migrations/202105231425045_mig1.cs
@@ -0,0 +1,40 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Products", "ProductCategory", c => c.String());
+            AddColumn("dbo.Products", "Description", c => c.String());
+            AddColumn("dbo.Products", "Category_Id", c => c.Int());
+            AlterColumn("dbo.Products", "Brand", c => c.String());
+            CreateIndex("dbo.Products", "Category_Id");
+            AddForeignKey("dbo.Products", "Category_Id", "dbo.Categories", "Id");
+            DropColumn("dbo.Products", "Category");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "Category", c => c.String());
+            DropForeignKey("dbo.Products", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "Category_Id" });
+            AlterColumn("dbo.Products", "Brand", c => c.Int(nullable: false));
+            DropColumn("dbo.Products", "Category_Id");
+            DropColumn("dbo.Products", "Description");
+            DropColumn("dbo.Products", "ProductCategory");
+            DropTable("dbo.Categories");
+        }
+    }
+}
