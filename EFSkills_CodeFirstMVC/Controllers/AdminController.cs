36061858e75f0f1532e225dfc997c15d25e38b7a
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-24 15:17:48 +0300

    added admin panel with listing of items

--- EFSkills_CodeFirstMVC/Controllers/AdminController.cs
+++ EFSkills_CodeFirstMVC/Controllers/AdminController.cs
@@ -1,12 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using EFSkills_CodeFirstMVC.Models.Classes;
 
 namespace EFSkills_CodeFirstMVC.Controllers
 {
-    public class DefaultController : Controller
+    public class AdminController : Controller
     {
-        // GET: Default
+        // GET: Admin
         public ActionResult Index()
         {
             Context c = new Context();
