4629d819f37e71d782fa202a7b622e6d89946e3a
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-25 14:00:07 +0300

    added edit and delete table items

--- EFSkills_CodeFirstMVC/Controllers/AdminController.cs
+++ EFSkills_CodeFirstMVC/Controllers/AdminController.cs
@@ -9,12 +9,47 @@ namespace EFSkills_CodeFirstMVC.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        Context c = new Context();
         public ActionResult Index()
         {
-            Context c = new Context();
             var values = c.Skills.ToList();
             return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(Skill s)
+        {
+            c.Skills.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var skillToDel = c.Skills.Find(id);
+            c.Skills.Remove(skillToDel);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var skillToUpd = c.Skills.Find(id);
+            return View("UpdateSkill",skillToUpd);
+        }       
+        [HttpPost]
+        public ActionResult UpdateSkill(Skill s)
+        {
+            var skillToUpd = c.Skills.Find(s.Id);
+            skillToUpd.Desc = s.Desc;
+            skillToUpd.Value = s.Value;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
